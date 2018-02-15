887ce4780d2f3c636ca0ed0788db28a21d78d220
= <joeyscarim@gmail.com>
2018-02-15 10:22:48 -0700

    Initial Commit

---
+++ QuickWeather/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  QuickWeather
+//
+//  Created by Joey Scarim on 2/15/18.
+//  Copyright © 2018 Joey Scarim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
